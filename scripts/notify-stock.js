6d49b62325533b4a8a7be4d1b71a8dc960801b0a
elvinpeng <elvinpeng@tencent.com>
2024-01-16 10:39:04 +0800

    feat: 使用 standard 替换 xo

--- scripts/notify-stock.js
+++ scripts/notify-stock.js
@@ -1,33 +1,32 @@
-import esMain from 'es-main';
-import {stocks} from 'stock-api';
-// eslint-disable-next-line import/no-unassigned-import
-import 'dotenv/config';
-import {checkProcessEnv, formatPercent, notify} from '../util/index.js';
+import esMain from 'es-main'
+import { stocks } from 'stock-api'
+import 'dotenv/config'
+import { checkProcessEnv, formatPercent, notify } from '../util/index.js'
 
-const ICON = 'https://gtimg.wechatpay.cn/resource/wxpay_oversea/2024-01-14/1705223028_1705223027981.png';
-const GROUP = '股票详情';
+const ICON = 'https://gtimg.wechatpay.cn/resource/wxpay_oversea/2024-01-14/1705223028_1705223027981.png'
+const GROUP = '股票详情'
 
 // 需要关注的股票列表
 const stockList = [
   'SH000001', // 上证指数
   'SH000300', // 沪深 300
-  'HK00700', // 腾讯
-];
+  'HK00700' // 腾讯
+]
 
-checkProcessEnv();
+checkProcessEnv()
 
 /**
  * 获取股票信息
  * @param {string} stockId 股票代码
  */
-async function getStockInfo(stockId) {
+async function getStockInfo (stockId) {
   const {
     name,
     now,
     low,
     high,
-    percent,
-  } = await stocks.tencent.getStock(stockId);
+    percent
+  } = await stocks.tencent.getStock(stockId)
   return {
     icon: ICON,
     group: GROUP,
@@ -35,20 +34,18 @@ async function getStockInfo(stockId) {
     body: [
       `收盘价：${now}`,
       `涨跌幅：${formatPercent(percent)}`,
-      `振幅：${formatPercent((high / low) - 1)}`,
-    ].join('，'),
-  };
+      `振幅：${formatPercent((high / low) - 1)}`
+    ].join('，')
+  }
 }
 
-export default async function notifyStock() {
+export default async function notifyStock () {
   for (const stockId of stockList) {
-    // eslint-disable-next-line no-await-in-loop
-    const stockInfo = await getStockInfo(stockId);
-    // eslint-disable-next-line no-await-in-loop
-    await notify(stockInfo);
+    const stockInfo = await getStockInfo(stockId)
+    await notify(stockInfo)
   }
 }
 
 if (esMain(import.meta)) {
-  await notifyStock();
+  await notifyStock()
 }
--- util/index.js
+++ util/index.js
@@ -1,5 +1,5 @@
-import process from 'node:process';
-import got from 'got';
+import process from 'node:process'
+import got from 'got'
 
 /**
  * 将数字格式化为百分比
@@ -7,24 +7,24 @@ import got from 'got';
  * @param {number} number 数字
  * @returns {string}
  */
-function formatPercent(number) {
-  return `${(number * 100).toFixed(2)}%`;
+function formatPercent (number) {
+  return `${(number * 100).toFixed(2)}%`
 }
 
 /**
  * 检查环境变量是否配置
  * 若未配置则抛出错误
  */
-function checkProcessEnv() {
-  const processEnv = process.env;
-  const requiredEnvs = ['BARK_KEY'];
+function checkProcessEnv () {
+  const processEnv = process.env
+  const requiredEnvs = ['BARK_KEY']
   for (const env of requiredEnvs) {
     if (processEnv[env] === undefined) {
-      throw new Error(`process.env.${env} is undefined`);
+      throw new Error(`process.env.${env} is undefined`)
     }
 
     if (processEnv[env] === '') {
-      throw new Error(`process.env.${env} is empty string`);
+      throw new Error(`process.env.${env} is empty string`)
     }
   }
 }
@@ -34,12 +34,12 @@ function checkProcessEnv() {
  * @param {object} message 提醒内容
  * @param {string} key 推送密钥
  */
-function notify(message, key = process.env.BARK_KEY) {
-  return got.post(`https://api.day.app/${key}`, {json: message}).json();
+function notify (message, key = process.env.BARK_KEY) {
+  return got.post(`https://api.day.app/${key}`, { json: message }).json()
 }
 
 export {
   formatPercent,
   checkProcessEnv,
-  notify,
-};
+  notify
+}
